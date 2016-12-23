b8d233b73312aa96c87f6694447d0074be732a56
angcyo <angcyo@126.com>
2016-12-23 15:54:58 +0800

    add swipe recycler view

--- uiview/src/main/java/com/angcyo/uiview/github/utilcode/utils/SnackbarUtils.java
+++ uiview/src/main/java/com/angcyo/uiview/github/utilcode/utils/SnackbarUtils.java
@@ -8,7 +8,7 @@ import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
-import com.blankj.utilcode.R;
+import com.angcyo.uiview.R;
 
 import java.lang.ref.WeakReference;
 
