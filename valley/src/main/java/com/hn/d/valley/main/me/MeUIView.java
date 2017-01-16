d3303da83b4d7e817e0a41cda6cde423bef30d69
angcyo <angcyo@126.com>
2017-01-16 17:02:44 +0800

    fix base

--- valley/src/main/java/com/hn/d/valley/main/me/MeUIView.java
+++ valley/src/main/java/com/hn/d/valley/main/me/MeUIView.java
@@ -1,6 +1,5 @@
 package com.hn.d.valley.main.me;
 
-import android.animation.ArgbEvaluator;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -19,7 +18,6 @@ import com.angcyo.uiview.net.RSubscriber;
 import com.angcyo.uiview.utils.RUtils;
 import com.angcyo.uiview.utils.T_;
 import com.angcyo.uiview.widget.ItemInfoLayout;
-import com.angcyo.uiview.widget.TitleBarLayout;
 import com.angcyo.uiview.widget.viewpager.TextIndicator;
 import com.facebook.drawee.view.SimpleDraweeView;
 import com.hn.d.valley.R;
@@ -50,14 +48,10 @@ import rx.functions.Action1;
  * Version: 1.0.0
  */
 public class MeUIView extends BaseUIView {
-    @BindView(R.id.title_view)
-    TextView mTitleView;
     @BindView(R.id.view_pager)
     ViewPager mViewPager;
     @BindView(R.id.single_text_indicator_view)
     TextIndicator mTextIndicatorView;
-    @BindView(R.id.title_bar_layout)
-    TitleBarLayout mTitleBarLayout;
     @BindView(R.id.user_ico_view)
     SimpleDraweeView mUserIcoView;
 
@@ -82,7 +76,16 @@ public class MeUIView extends BaseUIView {
 
     @Override
     protected TitleBarPattern getTitleBar() {
-        return null;
+        return super.getTitleBar()
+                .setFloating(true).setTitleString("")
+                .setTitleBarBGColor(Color.TRANSPARENT)
+                .addRightItem(TitleBarPattern.TitleBarItem.build(R.drawable.editor, new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        /* 编辑资料 */
+                        T_.show("编辑");
+                    }
+                }));
     }
 
     @Override
@@ -103,7 +106,7 @@ public class MeUIView extends BaseUIView {
             @Override
             public void call(LoginBean loginBean) {
                 //mFollowerNumView.setText(loginBean.get);
-                mTitleView.setText(loginBean.getUsername());
+                setTitleString(loginBean.getUsername());
                 UserControl.setUserId(mUserIdView, loginBean.getUid());
                 DraweeViewUtil.setDraweeViewHttp(mUserIcoView, loginBean.getAvatar());
                 mPersonAuthLayout.setItemDarkText("未认证");
@@ -122,9 +125,7 @@ public class MeUIView extends BaseUIView {
         mScrollRootLayout.setOnScrollChangeListener(new NestedScrollView.OnScrollChangeListener() {
             @Override
             public void onScrollChange(NestedScrollView v, int scrollX, int scrollY, int oldScrollX, int oldScrollY) {
-                mTitleBarLayout.setBackgroundColor((Integer) new ArgbEvaluator()
-                        .evaluate(Math.min(1, 6 * scrollY * 0.1f / mTitleBarLayout.getMeasuredHeight()),
-                                Color.TRANSPARENT, mActivity.getResources().getColor(R.color.colorPrimary)));
+                getUITitleBarContainer().evaluateBackgroundColorSelf(scrollY);
             }
         });
     }
@@ -169,14 +170,6 @@ public class MeUIView extends BaseUIView {
                     });
         }
         super.onViewShow(bundle);
-    }
-
-    /**
-     * 编辑资料
-     */
-    @OnClick(R.id.editor_view)
-    public void onEditorClick() {
-        T_.show("编辑");
     }
 
     @OnClick({R.id.qr_code_view, R.id.follow_item_layout, R.id.follower_item_layout})
