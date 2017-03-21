263a62c161c956079ea167c05af7bbe052803d86
angcyo <angcyo@126.com>
2017-03-21 14:18:08 +0800

    add custom title layout

--- valley/src/main/java/com/hn/d/valley/sub/user/NewFriend2UIView.java
+++ valley/src/main/java/com/hn/d/valley/sub/user/NewFriend2UIView.java
@@ -3,6 +3,7 @@ package com.hn.d.valley.sub.user;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.RelativeLayout;
 
 import com.angcyo.uiview.container.UILayoutImpl;
@@ -13,7 +14,6 @@ import com.angcyo.uiview.model.TitleBarPattern;
 import com.hn.d.valley.R;
 import com.hn.d.valley.base.BaseContentUIView;
 import com.hn.d.valley.cache.UserCache;
-import com.hn.d.valley.sub.other.FollowersRecyclerUIView;
 import com.hn.d.valley.sub.other.FriendsRecommendUIView;
 
 import butterknife.BindView;
@@ -23,10 +23,8 @@ import butterknife.BindView;
  */
 public final class NewFriend2UIView extends BaseContentUIView {
 
-
     @BindView(R.id.main_new_friend)
     UILayoutImpl mContainerLayout;
-    @BindView(R.id.tablayout_new_friend)
     SegmentTabLayout tabLayout;
 
     private FriendsNewUIView mFollowersUIView;
@@ -34,38 +32,21 @@ public final class NewFriend2UIView extends BaseContentUIView {
 
     private int lastPosition = -1;
 
-    public NewFriend2UIView() {}
+    public NewFriend2UIView() {
+    }
 
     @Override
     protected void inflateContentLayout(RelativeLayout baseContentLayout, LayoutInflater inflater) {
         inflate(R.layout.view_new_friend_2);
-
     }
 
     @Override
     protected void initOnShowContentLayout() {
         super.initOnShowContentLayout();
-        initTablayout();
-
-    }
-
-    @Override
-    public void onViewShow(Bundle bundle) {
-        super.onViewShow(bundle);
-
-        if (mFollowersUIView == null) {
-            mFollowersUIView = new FriendsNewUIView();
-            mFollowersUIView.bindOtherILayout(mOtherILayout);
-            mContainerLayout.startIView(mFollowersUIView, new UIParam(false));
-        } else {
-            mContainerLayout.showIView(mFollowersUIView, false);
-        }
-
-        lastPosition = 0;
     }
 
     private void initTablayout() {
-        tabLayout.setTabData(new String[]{"新的朋友","好友推荐"});
+        tabLayout.setTabData(new String[]{getString(R.string.new_frient), getString(R.string.frient_tip)});
         tabLayout.setCurrentTab(0);
         tabLayout.setOnTabSelectListener(new OnTabSelectListener() {
             @Override
@@ -104,6 +85,7 @@ public final class NewFriend2UIView extends BaseContentUIView {
                 mContainerLayout.showIView(mFriendsRecommUIView);
             }
         }
+        lastPosition = position;
     }
 
 
@@ -116,11 +98,30 @@ public final class NewFriend2UIView extends BaseContentUIView {
     @Override
     public void onViewShowFirst(Bundle bundle) {
         super.onViewShowFirst(bundle);
+        if (mFollowersUIView == null) {
+            mFollowersUIView = new FriendsNewUIView();
+            mFollowersUIView.bindOtherILayout(mOtherILayout);
+            mContainerLayout.startIView(mFollowersUIView, new UIParam(false));
+        } else {
+            mContainerLayout.showIView(mFollowersUIView, false);
+        }
+
+        lastPosition = 0;
     }
 
     @Override
     protected TitleBarPattern getTitleBar() {
-        return null;
+        return super.getTitleBar()
+                .setShowBackImageView(true)
+                .setTitleString("")
+                .setOnInitTitleLayout(new TitleBarPattern.OnInitTitleLayout() {
+                    @Override
+                    public void onInitLayout(RelativeLayout parent) {
+                        View.inflate(mActivity, R.layout.item_segment_layout, parent);
+                        tabLayout = (SegmentTabLayout) parent.findViewById(R.id.tablayout_new_friend);
+                        initTablayout();
+                    }
+                });
     }
 
 
